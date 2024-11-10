2e5df15af6a344da9b757d48c683df91d32d57cf
Михаил Якименко <rik.zunqq@gmail.com>
2024-11-11 02:09:29 +0300

    text stats fix

--- src/components/TextStats/TextStats.tsx
+++ src/components/TextStats/TextStats.tsx
@@ -8,12 +8,14 @@ export function TextStats(props: TextStatsProps) {
     const textLength = props.text.length
     const textSize = new Blob([props.text]).size
 
-    const compLength = props.comparable && Math.floor(((textLength / props.comparable.length) * 100) - 100)
-    const compSize = props.comparable && Math.floor(((textSize / new Blob([props.comparable]).size) * 100) - 100)
+    const compLength = props.comparable ? Math.floor(((textLength / props.comparable.length) * 100) - 100) : null
+    const compLengthSign = (compLength ?? 0) >= 0 ? '+' : ''
+    const compSize = props.comparable ? Math.floor(((textSize / new Blob([props.comparable]).size) * 100) - 100) : null
+    const compSizeSign = (compSize ?? 0) >= 0 ? '+' : ''
 
     return (
         <span className="text-stats">
-            {textLength}{compLength && `(+${compLength}%)`} chars {textSize}{compSize && `(+${compSize}%)`} bytes
+            {textLength}{compLength && `(${compLengthSign}${compLength}%)`} chars {textSize}{compSize && `(${compSizeSign}${compSize}%)`} bytes
         </span>
     )
 }
