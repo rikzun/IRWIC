fefcd7e87366d48b071b910ca2e7c95282610966
Михаил Якименко <rik.zunqq@gmail.com>
2024-11-14 00:06:22 +0300

    template update

--- declarations.d.ts
+++ declarations.d.ts
@@ -1,35 +1,36 @@
-declare module "*.png" {
+declare module '*.png' {
     const path: string
     export default path
 }
 
-declare module "*.jpg" {
+declare module '*.jpg' {
     const path: string
     export default path
 }
 
-declare module "*.jpeg" {
+declare module '*.jpeg' {
     const path: string
     export default path
 }
 
-declare module "*.gif" {
+declare module '*.gif' {
     const path: string
     export default path
 }
 
-declare module "*.webp" {
+declare module '*.webp' {
     const path: string
     export default path
 }
 
-declare module "*.ico" {
+declare module '*.ico' {
     const path: string
     export default path
 }
 
-declare module "*.svg" {
-    import React from "react"
-    const SVG: React.FC<React.SVGProps<SVGSVGElement>>
-    export default SVG
+declare module '*.svg' {
+    import type { FunctionComponent, SVGProps } from "react"
+
+    const component: FunctionComponent<SVGProps<SVGSVGElement>>
+    export default component
 }
